24abe8aa4312fdc64f86a266755976aa01204c80
ashma <67820169+ashmaghimire8@users.noreply.github.com>
2022-09-28 15:04:55 +0545

    Update data.py

--- data.py
+++ data.py
@@ -1,3 +1,4 @@
+# questions from internet (did it for a-branch)
 question_data = [
     {"text": "A slug's blood is green.", "answer": "True"},
     {"text": "The loudest animal is the African Elephant.", "answer": "False"},
